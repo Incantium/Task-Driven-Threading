12969a0ea02fbb7ae2990e96f453ab796cce4278
TGerritsen98 <vanaest@outlook.com>
2024-05-23 21:08:11 +0200

    Moved schedule methods in extension class
    This enables the use of var for tasks (except when the class overrides multiple task interfaces).

--- Runtime/MultiTask.cs
+++ Runtime/MultiTask.cs
@@ -9,7 +9,7 @@ namespace Obscurum.TDT
     /// <typeparam name="T">The return typing of the task.</typeparam>
     /// <author>Vanaest</author>
     /// <version>1.0.0</version>
-    public interface MultiTask<T>
+    public interface MultiTask<out T>
     {
         /// <summary>
         /// Method called to execute the <see cref="MultiTask{T}"/>.
@@ -17,120 +17,6 @@ namespace Obscurum.TDT
         /// <param name="i">The index of the current single task in the <see cref="MultiTask{T}"/>.</param>
         /// <returns>The return value of the <see cref="MultiTask{T}"/>.</returns>
         T Execute(int i);
-        
-        /// <summary>
-        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
-        /// </summary>
-        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>.</param>
-        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
-        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
-        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
-        /// milliseconds.</param>
-        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
-        /// </returns>
-        public Tracker<T[]> Schedule(
-            int amount, 
-            int batch = 1, 
-            int timeout = 0)
-        {
-            var tracker = new Tracker<T[]>();
-            
-            BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner<T>(this, tracker, batch, timeout) : 
-                new MultiRunner<T>(this, tracker, batch);
-            
-            runner.Start(amount); 
-
-            return tracker;
-        }
-        
-        /// <summary>
-        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
-        /// </summary>
-        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>. This value is
-        /// calculated dynamically.</param>
-        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
-        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
-        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
-        /// milliseconds.</param>
-        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
-        /// </returns>
-        public Tracker<T[]> Schedule(
-            ICollection amount,
-            int batch = 1,
-            int timeout = 0) 
-        {
-            var tracker = new Tracker<T[]>();
-            
-            BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner<T>(this, tracker, batch, timeout) : 
-                new MultiRunner<T>(this, tracker, batch);
-            
-            runner.Start(amount.Count); 
-
-            return tracker;
-        }
-        
-        /// <summary>
-        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
-        /// </summary>
-        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>.</param>
-        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask{T}"/> depends upon.</param>
-        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
-        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
-        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
-        /// milliseconds.</param>
-        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
-        /// </returns>
-        public Tracker<T[]> Schedule(
-            int amount, 
-            Tracker dependency, 
-            int batch = 1, 
-            int timeout = 0)
-        {
-            var tracker = new Tracker<T[]>();
-            
-            BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner<T>(this, tracker, batch, timeout) : 
-                new MultiRunner<T>(this, tracker, batch);
-            
-            dependency.dependency += () => runner.Start(amount);
-            
-            return tracker;
-        }
-        
-        /// <summary>
-        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
-        /// </summary>
-        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>. This value is
-        /// calculated dynamically.</param>
-        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask{T}"/> depends upon.</param>
-        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
-        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
-        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
-        /// milliseconds.</param>
-        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
-        /// </returns>
-        public Tracker<T[]> Schedule(
-            ICollection amount,
-            Tracker dependency,
-            int batch = 1, 
-            int timeout = 0) 
-        {
-            var tracker = new Tracker<T[]>();
-            
-            BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner<T>(this, tracker, batch, timeout) : 
-                new MultiRunner<T>(this, tracker, batch);
-            
-            dependency.dependency += () => runner.Start(amount.Count);
-
-            return tracker;
-        }
     }
     
     /// <summary>
@@ -145,17 +31,150 @@ namespace Obscurum.TDT
         /// </summary>
         /// <param name="i">The index of the current single task in the <see cref="MultiTask"/>.</param>
         void Execute(int i);
+    }
+
+    /// <summary>
+    /// Extension class for scheduling the <see cref="MultiTask"/> and <see cref="MultiTask{T}"/> interface for
+    /// execution.
+    /// </summary>
+    /// <author>Vanaest</author>
+    /// <version>1.0.0</version>
+    public static class MultiTaskScheduler
+    {
+        /// <summary>
+        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="MultiTask{T}"/>.</param>
+        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>.</param>
+        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
+        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
+        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
+        /// milliseconds.</param>
+        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
+        /// </returns>
+        public static Tracker<T[]> Schedule<T>(
+            this MultiTask<T> task,
+            int amount, 
+            int batch = 1, 
+            int timeout = 0)
+        {
+            var tracker = new Tracker<T[]>();
+            
+            BaseMultiRunner runner = timeout > 0 ? 
+                new MultiTimeoutRunner<T>(task, tracker, batch, timeout) : 
+                new MultiRunner<T>(task, tracker, batch);
+            
+            runner.Start(amount); 
+
+            return tracker;
+        }
+        
+        /// <summary>
+        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="MultiTask{T}"/>.</param>
+        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>. This value is
+        /// calculated dynamically.</param>
+        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
+        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
+        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
+        /// milliseconds.</param>
+        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
+        /// </returns>
+        public static Tracker<T[]> Schedule<T>(
+            this MultiTask<T> task,
+            ICollection amount,
+            int batch = 1,
+            int timeout = 0) 
+        {
+            var tracker = new Tracker<T[]>();
+            
+            BaseMultiRunner runner = timeout > 0 ? 
+                new MultiTimeoutRunner<T>(task, tracker, batch, timeout) : 
+                new MultiRunner<T>(task, tracker, batch);
+            
+            runner.Start(amount.Count); 
+
+            return tracker;
+        }
+        
+        /// <summary>
+        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="MultiTask{T}"/>.</param>
+        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>.</param>
+        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask{T}"/> depends upon.</param>
+        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
+        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
+        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
+        /// milliseconds.</param>
+        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
+        /// </returns>
+        public static Tracker<T[]> Schedule<T>(
+            this MultiTask<T> task,
+            int amount, 
+            Tracker dependency, 
+            int batch = 1, 
+            int timeout = 0)
+        {
+            var tracker = new Tracker<T[]>();
+            
+            BaseMultiRunner runner = timeout > 0 ? 
+                new MultiTimeoutRunner<T>(task, tracker, batch, timeout) : 
+                new MultiRunner<T>(task, tracker, batch);
+            
+            dependency.dependency += () => runner.Start(amount);
+            
+            return tracker;
+        }
+        
+        /// <summary>
+        /// Method to schedule a <see cref="MultiTask{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="MultiTask{T}"/>.</param>
+        /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask{T}"/>. This value is
+        /// calculated dynamically.</param>
+        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask{T}"/> depends upon.</param>
+        /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
+        /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
+        /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
+        /// milliseconds.</param>
+        /// <typeparam name="T">The typing of the <see cref="MultiTask{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="MultiTask{T}"/>.
+        /// </returns>
+        public static Tracker<T[]> Schedule<T>(
+            this MultiTask<T> task,
+            ICollection amount,
+            Tracker dependency,
+            int batch = 1, 
+            int timeout = 0) 
+        {
+            var tracker = new Tracker<T[]>();
+            
+            BaseMultiRunner runner = timeout > 0 ? 
+                new MultiTimeoutRunner<T>(task, tracker, batch, timeout) : 
+                new MultiRunner<T>(task, tracker, batch);
+            
+            dependency.dependency += () => runner.Start(amount.Count);
+
+            return tracker;
+        }
         
         /// <summary>
         /// Method to schedule a <see cref="MultiTask"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="MultiTask"/>.</param>
         /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask"/>.</param>
         /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
         /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
         /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
         /// milliseconds.</param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="MultiTask"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this MultiTask task,
             int amount, 
             int batch = 1, 
             int timeout = 0)
@@ -163,8 +182,8 @@ namespace Obscurum.TDT
             var tracker = new Tracker();
             
             BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner(this, tracker, batch, timeout) : 
-                new MultiRunner(this, tracker, batch);
+                new MultiTimeoutRunner(task, tracker, batch, timeout) : 
+                new MultiRunner(task, tracker, batch);
             
             runner.Start(amount); 
             
@@ -174,6 +193,7 @@ namespace Obscurum.TDT
         /// <summary>
         /// Method to schedule a <see cref="MultiTask"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="MultiTask"/>.</param>
         /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask"/>. This value is
         /// calculated dynamically.</param>
         /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
@@ -181,7 +201,8 @@ namespace Obscurum.TDT
         /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
         /// milliseconds.</param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="MultiTask"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this MultiTask task,
             ICollection amount,
             int batch = 1, 
             int timeout = 0) 
@@ -189,8 +210,8 @@ namespace Obscurum.TDT
             var tracker = new Tracker();
             
             BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner(this, tracker, batch, timeout) : 
-                new MultiRunner(this, tracker, batch);
+                new MultiTimeoutRunner(task, tracker, batch, timeout) : 
+                new MultiRunner(task, tracker, batch);
             
             runner.Start(amount.Count); 
 
@@ -200,14 +221,16 @@ namespace Obscurum.TDT
         /// <summary>
         /// Method to schedule a <see cref="MultiTask"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="MultiTask"/>.</param>
         /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask"/>.</param>
-        /// /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask"/> depends upon.</param>
+        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask"/> depends upon.</param>
         /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
         /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
         /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
         /// milliseconds.</param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="MultiTask"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this MultiTask task,
             int amount, 
             Tracker dependency, 
             int batch = 1, 
@@ -216,8 +239,8 @@ namespace Obscurum.TDT
             var tracker = new Tracker();
             
             BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner(this, tracker, batch, timeout) : 
-                new MultiRunner(this, tracker, batch);
+                new MultiTimeoutRunner(task, tracker, batch, timeout) : 
+                new MultiRunner(task, tracker, batch);
             
             dependency.dependency += () => runner.Start(amount);
             
@@ -227,15 +250,17 @@ namespace Obscurum.TDT
         /// <summary>
         /// Method to schedule a <see cref="MultiTask"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="MultiTask"/>.</param>
         /// <param name="amount">The amount of single tasks to run in the <see cref="MultiTask"/>. This value is
         /// calculated dynamically.</param>
-        /// /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask"/> depends upon.</param>
+        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="MultiTask"/> depends upon.</param>
         /// <param name="batch">The batch size or amount of single tasks run per thread. The amount of threads started
         /// is the <see cref="amount"/> divided by the <see cref="batch"/> size, rounded up.</param>
         /// <param name="timeout">The maximum allotted time for a <see cref="batch"/> of single tasks to take in
         /// milliseconds.</param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="MultiTask"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this MultiTask task,
             ICollection amount,
             Tracker dependency,
             int batch = 1, 
@@ -244,8 +269,8 @@ namespace Obscurum.TDT
             var tracker = new Tracker();
             
             BaseMultiRunner runner = timeout > 0 ? 
-                new MultiTimeoutRunner(this, tracker, batch, timeout) : 
-                new MultiRunner(this, tracker, batch);
+                new MultiTimeoutRunner(task, tracker, batch, timeout) : 
+                new MultiRunner(task, tracker, batch);
             
             dependency.dependency += () => runner.Start(amount.Count);
 
--- Runtime/Task.cs
+++ Runtime/Task.cs
@@ -8,57 +8,13 @@ namespace Obscurum.TDT
     /// <typeparam name="T">The return typing of the task.</typeparam>
     /// <author>Vanaest</author>
     /// <version>1.0.0</version>
-    public interface Task<T>
+    public interface Task<out T>
     {
         /// <summary>
         /// Method called to execute the <see cref="Task{T}"/>.
         /// </summary>
         /// <returns>The return value of the <see cref="Task{T}"/>.</returns>
         T Execute();
-        
-        /// <summary>
-        /// Method to schedule a <see cref="Task{T}"/> for execution.
-        /// </summary>
-        /// <param name="timeout">The maximum allotted time for the <see cref="Task{T}"/> to take in milliseconds.
-        /// </param>
-        /// <typeparam name="T">The typing of the <see cref="Task{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="Task{T}"/>.</returns>
-        public Tracker<T> Schedule(
-            int timeout = 0)
-        {
-            var tracker = new Tracker<T>();
-            
-            BaseRunner runner = timeout > 0 ? 
-                new TimeoutRunner<T>(this, tracker, timeout) : 
-                new Runner<T>(this, tracker);
-            
-            runner.Start(); 
-
-            return tracker;
-        }
-        
-        /// <summary>
-        /// Method to schedule a <see cref="Task{T}"/> for execution.
-        /// </summary>
-        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="Task{T}"/> depends upon.</param>
-        /// <param name="timeout">The maximum allotted time for the <see cref="Task{T}"/> to take in milliseconds.
-        /// </param>
-        /// <typeparam name="T">The typing of the <see cref="Task{T}"/>.</typeparam>
-        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="Task{T}"/>.</returns>
-        public Tracker<T> Schedule(
-            Tracker dependency,
-            int timeout = 0)
-        {
-            var tracker = new Tracker<T>();
-            
-            BaseRunner runner = timeout > 0 ? 
-                new TimeoutRunner<T>(this, tracker, timeout) : 
-                new Runner<T>(this, tracker);
-            
-            dependency.dependency += () => runner.Start();
-
-            return tracker;
-        }
     }
     
     /// <summary>
@@ -72,21 +28,79 @@ namespace Obscurum.TDT
         /// Method called to execute the <see cref="Task"/>.
         /// </summary>
         void Execute();
+    }
+    
+    /// <summary>
+    /// Extension class for scheduling the <see cref="Task"/> and <see cref="Task{T}"/> interface for execution.
+    /// </summary>
+    /// <author>Vanaest</author>
+    /// <version>1.0.0</version>
+    public static class TaskScheduler
+    {
+        /// <summary>
+        /// Method to schedule a <see cref="Task{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="Task{T}"/>.</param>
+        /// <param name="timeout">The maximum allotted time for the <see cref="Task{T}"/> to take in milliseconds.
+        /// </param>
+        /// <typeparam name="T">The typing of the <see cref="Task{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="Task{T}"/>.</returns>
+        public static Tracker<T> Schedule<T>(
+            this Task<T> task,
+            int timeout = 0)
+        {
+            var tracker = new Tracker<T>();
+            
+            BaseRunner runner = timeout > 0 ? 
+                new TimeoutRunner<T>(task, tracker, timeout) : 
+                new Runner<T>(task, tracker);
+            
+            runner.Start(); 
+
+            return tracker;
+        }
+        
+        /// <summary>
+        /// Method to schedule a <see cref="Task{T}"/> for execution.
+        /// </summary>
+        /// <param name="task">The <see cref="Task{T}"/>.</param>
+        /// <param name="dependency">The <see cref="Tracker"/> this <see cref="Task{T}"/> depends upon.</param>
+        /// <param name="timeout">The maximum allotted time for the <see cref="Task{T}"/> to take in milliseconds.
+        /// </param>
+        /// <typeparam name="T">The typing of the <see cref="Task{T}"/>.</typeparam>
+        /// <returns>A <see cref="Tracker{T}"/> to keep track of the progress of the <see cref="Task{T}"/>.</returns>
+        public static Tracker<T> Schedule<T>(
+            this Task<T> task,
+            Tracker dependency,
+            int timeout = 0)
+        {
+            var tracker = new Tracker<T>();
+            
+            BaseRunner runner = timeout > 0 ? 
+                new TimeoutRunner<T>(task, tracker, timeout) : 
+                new Runner<T>(task, tracker);
+            
+            dependency.dependency += () => runner.Start();
+
+            return tracker;
+        }
         
         /// <summary>
         /// Method to schedule a <see cref="Task"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="Task"/>.</param>
         /// <param name="timeout">The maximum allotted time for the <see cref="Task"/> to take in milliseconds.
         /// </param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="Task"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this Task task,
             int timeout = 0)
         {
             var tracker = new Tracker();
             
             BaseRunner runner = timeout > 0 ? 
-                new TimeoutRunner(this, tracker, timeout) : 
-                new Runner(this, tracker);
+                new TimeoutRunner(task, tracker, timeout) : 
+                new Runner(task, tracker);
             
             runner.Start(); 
 
@@ -96,19 +110,21 @@ namespace Obscurum.TDT
         /// <summary>
         /// Method to schedule a <see cref="Task"/> for execution.
         /// </summary>
+        /// <param name="task">The <see cref="Task"/>.</param>
         /// <param name="dependency">The <see cref="Tracker"/> this <see cref="Task"/> depends upon.</param>
         /// <param name="timeout">The maximum allotted time for the <see cref="Task"/> to take in milliseconds.
         /// </param>
         /// <returns>A <see cref="Tracker"/> to keep track of the progress of the <see cref="Task"/>.</returns>
-        public Tracker Schedule(
+        public static Tracker Schedule(
+            this Task task,
             Tracker dependency,
             int timeout = 0)
         {
             var tracker = new Tracker();
             
             BaseRunner runner = timeout > 0 ? 
-                new TimeoutRunner(this, tracker, timeout) : 
-                new Runner(this, tracker);
+                new TimeoutRunner(task, tracker, timeout) : 
+                new Runner(task, tracker);
             
             dependency.dependency += () => runner.Start();
 
